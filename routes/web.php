b5b771eae65ba72cf3343b5b6db676fc0ad144b1
SebasBaezCode <jsbaez5@misena.edu.co>
2018-06-14 10:54:35 -0500

    Test

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,30 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+
+Route::get('/users',function(){
+    return 'Users';
+});
+
+Route::get('/users/{id}',function($id){
+    //return 'Showing user details: ' .$_GET['id'];
+    return "Showing user details: {$id}";
+})->where("id","[0-9]+");//Beacuse this view only accepts numbers and is not confused with 'newUser'
+
+Route::get('/users/newUser',function(){
+    return "Creating a new user";
+});
+
+Route::get('/greeting/{name}/{nickName?}',function($name,$nickName = null){
+    if($nickName)
+    {
+        $msg = "Welcome {$name}, your nickname is: {$nickName}";
+    }
+    else
+    {
+        $msg = "Welcome {$name}, you haven't nickname";
+    }
+    return $msg;
 });
---
+++ tests/Feature/UsersModuleTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UsersModuleTest extends TestCase
+{
+     /** @test */ 
+    function loadTheUsers()
+    {
+        $this->get('/users')
+        ->assertStatus(200)
+        ->assertSee('Users');
+    }
+
+    /** @test */
+    function loadUserDetails()
+    {
+    	$this->get('/users/1235648')
+    	->assertStatus(200)
+    	->assertSee("Showing user details: 1235648");
+    }
+
+    /** @test */
+    function loadNewUser()
+    {
+    	$this->get('/users/newUser')
+    	->assertStatus(200)
+    	->assertSee("Creating a new user");
+    }
+
+    /** @test */
+    function loadUserGreeting1()
+    {
+    	$this->get('/greeting/UserNumber_1/Number_1')
+    	->assertStatus(200)
+    	->assertSee("Welcome UserNumber_1, your nickname is: Number_1");
+    }
+
+    /** @test */
+    function loadUserGreeting2()
+    {
+    	$this->get('/greeting/UserNumber_1/')
+    	->assertStatus(200)
+    	->assertSee("Welcome UserNumber_1, you haven't nickname");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
